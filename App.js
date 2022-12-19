526843d6e6f5bf578088530670738efb79955835
Alina Bulatova <75022064+alinaBR@users.noreply.github.com>
2022-12-20 00:05:33 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,72 @@
+import  React from 'react';
+import Task from './components/Task';
+import TaskInput from './components/TaskInput';
+
+
+class App extends React.Component {
+  constructor() {
+    super();
+
+
+    this.state = {
+      tasks: [
+        {id: 0, title: 'Create todo-react app', done: false},
+        {id: 1, title: 'Make a video about it', done: true},
+        {id: 2, title: 'Create simple todo-app', done: false}
+      ]
+    };
+  }
+
+  addTask = task => {
+    this.setState(state => {
+      let {tasks} = state;
+      tasks.push({
+        id: tasks.length !== 0 ? task.length : 0,
+        title: task,
+        done: false
+      });
+      return tasks;
+    });
+  };
+
+  doneTask = id => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+  this.setState(state => {
+    let {tasks} = state;
+    tasks[index].done = true;
+    return tasks;
+  });
+  };
+
+  deleteTask = id => {
+    const index = this.state.tasks.map(task => task.id).indexOf(id);
+  this.setState(state => {
+    let {tasks} = state;
+    delete tasks[index];
+    return tasks;
+    });
+  };
+  render() {
+    const { tasks } = this.state;
+    const activeTasks = tasks.filter(task => !task.done);
+    const doneTasks = tasks.filter(task => task.done);
+
+    return(
+      <div className="App">
+        <h1 className="top">Active tasks: {activeTasks.length}</h1>
+        {[...activeTasks, ...doneTasks].map(task => (
+          <Task
+           doneTask={() => this.doneTask(task.id)}
+           deleteTask={() => this.deleteTask(task.id)}
+           task={task}
+            key={task.id}
+            ></Task>
+        ))}
+
+        <TaskInput addTask={this.addTask}></TaskInput>
+
+      </div>
+    );
+  }
+}
+export default App;
